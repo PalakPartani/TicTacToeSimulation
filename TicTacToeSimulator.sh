ddf50ef5203b68a056620a9d50c81cc2e1f71afd
PalakPartani <partanipalak16@gmail.com>
2020-02-11 11:56:31 +0530

    ADDED:method to assign letter to player

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -6,7 +6,6 @@ readonly POSITION=9
 #initailize the board
 declare -a gameBoard
 
-
 #function to display the board.
 function displayBoard(){
 	for (( i=1; i<=$POSITION; i=$(($i+3)) ))
@@ -14,3 +13,15 @@ function displayBoard(){
 		echo "${gameBoard[i]} |  ${gameBoard[i+1]} | ${gameBoard[i+2]}"
 	done
 }
+
+#function to assign sign to player
+function assignSignToPlayer(){
+	assignedLetter=$(($((RANDOM%2))+1))
+	if [ $assignedLetter -eq 0 ]
+	then
+		userSign="X"
+	else
+		userSign="O"
+	fi
+}
+assignSignToPlayer
