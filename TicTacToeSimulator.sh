2f0b087dabc2abeb5ecd1644a9634e4d0b91fe43
PalakPartani <partanipalak16@gmail.com>
2020-02-11 11:47:39 +0530

    ADDED:array to reset board

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to TicTacToe game.Good Luck!"
+#declare constants
+readonly POSITION=9
+
+#initailize the board
+declare -a gameBoard
+
+
+#function to display the board.
+function displayBoard(){
+	for (( i=1; i<=$POSITION; i=$(($i+3)) ))
+	do
+		echo "${gameBoard[i]} |  ${gameBoard[i+1]} | ${gameBoard[i+2]}"
+	done
+}
